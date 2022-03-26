8be711639373ebcf1007c59cd7e5bad84d28a3a2
mgispert <mcg-511@hotmail.com>
2022-03-26 20:08:40 +0100

    feat: modify base cra files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import { store } from "./store";
+import { catleryTheme } from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ChakraProvider theme={catleryTheme}>
+    <ColorModeScript initialColorMode={catleryTheme.config.initialColorMode} />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
